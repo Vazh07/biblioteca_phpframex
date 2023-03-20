75a7b9b5fc793e198bd74dcd503275e509d4ce9c
Vazh07 <mozamorah@gmail.com>
2023-03-19 23:49:16 -0600

    Fixing routing

---
+++ routes.php
@@ -0,0 +1,15 @@
+<?php
+require_once('./controllers/authorController.php');
+require_once('./controllers/bookController.php');
+require_once('./controllers/publisherController.php');
+
+Route::get('/',function() { 
+    return view('main');
+});
+
+Route::resource('author','authorController');
+Route::resource('book','bookController');
+Route::resource('publisher','publisherController');
+
+Route::dispatch();
+?>
